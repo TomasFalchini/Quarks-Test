db7407161d1d812be2a48d4a8bd8478d299a5a78
Tomas Falchini <tomasfalchini@gmail.com>
2023-04-25 02:40:56 -0300

    feat(form.dto.ts): add validation to FormDto properties name, lastname, age, and birthday

--- backend/forms/src/form/dto/form.dto.ts
+++ backend/forms/src/form/dto/form.dto.ts
@@ -1,1 +1,30 @@
-export class FormDto {}
+import {
+  IsNotEmpty,
+  IsPositive,
+  IsDate,
+  IsString,
+  IsNumber,
+  Max,
+  Min,
+  MinDate,
+} from 'class-validator';
+
+export class FormDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  lastname: string;
+
+  @IsPositive()
+  @IsNumber()
+  @Max(100)
+  @Min(1)
+  age: number;
+
+  @IsDate()
+  @MinDate(new Date('01/01/1924'))
+  birthday: Date;
+}
