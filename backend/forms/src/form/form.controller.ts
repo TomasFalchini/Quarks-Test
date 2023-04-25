e21bf2cd17d0f892b8ea4e18b83c95a4c7ce18bc
Tomas Falchini <tomasfalchini@gmail.com>
2023-04-25 02:32:26 -0300

    feat(form.controller.ts): add error handling to saveData method with HttpException

--- backend/forms/src/form/form.controller.ts
+++ backend/forms/src/form/form.controller.ts
@@ -1,14 +1,26 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, HttpStatus } from '@nestjs/common';
 import { FormService } from './form.service';
 import { FormDto } from './dto/form.dto';
+import { HttpException } from '@nestjs/common/exceptions';
 
 @Controller('form')
 export class FormController {
   constructor(private readonly formService: FormService) {}
 
   @Post()
-  saveData(@Body() FormDto: FormDto) {
-    return this.formService.saveData(FormDto);
+  async saveData(@Body() FormDto: FormDto) {
+    try {
+      return await this.formService.saveData(FormDto);
+    } catch (err) {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'There must be something bad with the data you hace completed',
+        },
+        HttpStatus.BAD_REQUEST,
+        { cause: err },
+      );
+    }
   }
 
   @Get()
